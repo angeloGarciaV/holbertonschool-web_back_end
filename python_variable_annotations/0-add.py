4426551ecd80abc70c852d6377400e39b327c8d4
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-25 19:06:49 -0400

    Add type annotations to add function in 0-add.py

--- python_variable_annotations/0-add.py
+++ python_variable_annotations/0-add.py
@@ -1,2 +1,9 @@
 #!/usr/bin/python3
-""""""
+"""Module that takes two floats and returns their sum"""
+import typing
+
+
+def add(a: float, b: float):
+    """Adds two floats"""
+    sumOf: float = a + b
+    return sumOf
