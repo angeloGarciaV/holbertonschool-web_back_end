85a0ff94d3316e05f333809c6d82bd1f7d0378c5
angeloGarciaV <angelogarcia1029@gmail.com>
2024-04-25 22:45:29 -0400

    Executable

--- python_variable_annotations/1-concat.py
+++ python_variable_annotations/1-concat.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-"""Takes two strings and returns a concatenated string"""
+"""takes two strings and returns a concatenated string"""
 
 
 def concat(str1: str, str2: str) -> str:
