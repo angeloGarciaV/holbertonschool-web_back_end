6e4f0b495c75587fd6784aa629f02b983b04b659
angeloGarciaV <angelogarcia1029@gmail.com>
2024-04-25 22:48:52 -0400

    added 2-floor

---
+++ python_variable_annotations/2-floor.py
@@ -0,0 +1,8 @@
+#!/usr/bin/env python3
+""" Module for storing the floor function. """
+import math
+
+
+def floor(n: float) -> int:
+    """ Returns the floor of a float. """
+    return math.floor(n)
