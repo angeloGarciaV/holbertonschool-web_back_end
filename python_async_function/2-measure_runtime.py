86964fa2cd2018128b0bafbc286989cb86f5fbc6
Angelo Garcia <angelogarcia1029@gmail.com>
2024-04-26 08:15:21 -0400

    Refactor measure_time function to use asyncio.run in python_async_function/2-measure_runtime.py

--- python_async_function/2-measure_runtime.py
+++ python_async_function/2-measure_runtime.py
@@ -8,7 +8,6 @@ wait_n = __import__('1-concurrent_coroutines').wait_n
 def measure_time(n: int, max_delay: int) -> float:
     """Measures the runtime of an async comprehension"""
     start = time.time()
-    wait_n(n, max_delay)
+    asyncio.run(wait_n(n, max_delay))
     end = time.time()
-    total_time = end - start
-    return total_time / n
+    return (end - start) / n
