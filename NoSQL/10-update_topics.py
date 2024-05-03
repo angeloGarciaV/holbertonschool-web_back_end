a804fa8a04bdbd7659623361037eb51dfc4a9808
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-02 22:31:41 -0400

    Add NoSQL function to update topics of a school document

---
+++ NoSQL/10-update_topics.py
@@ -0,0 +1,9 @@
+#!/usr/bin/env python3
+"Function that changes all topics of a school document based on the name"
+import pymongo
+
+
+def update_topics(mongo_collection, name, topics):
+    """Function that changes all topics of a school document based on the name"""
+    query: dict = {"name": name}
+    mongo_collection.update_many(query, {"$set": {"topics": topics}})
