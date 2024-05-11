d769f131388b4955ae610913020c510240732e9a
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-10 23:44:40 -0400

    Refactor Currency class for better consistency and error handling

--- ES6_classes/3-currency.js
+++ ES6_classes/3-currency.js
@@ -1,26 +1,31 @@
 export default class Currency{
-  constructor(code = '', name = ''){
+  constructor(code = '', name = '') {
     this._code = code;
     this._name = name;
   }
+
   get code() {
     return this._code;
   }
+
   set code(value) {
-    if(typeof value === 'string'){
-      throw new TypeError('code must be a string');
+    if(typeof value === 'string') {
+      throw new TypeError('Code must be a string');
     }
     this._code = value;
   }
+
   get name() {
     return this._code;
   }
+
   set name(value) {
-    if(typeof value === 'string'){
-      throw new TypeError('name must be a string');
+    if(typeof value === 'string') {
+      throw new TypeError('Name must be a string');
     }
     this._name = value;
   }
+
   displayFullCurrency() {
     return `${this._name} (${this._code})`
   }
