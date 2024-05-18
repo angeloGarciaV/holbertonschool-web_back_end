04e8d85f02581eda25333afe222cd1995aa38f99
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-17 23:46:07 -0400

    fix for 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,4 +1,4 @@
-import signUpUser from './4-user-promise.js'
+import signUpUser from './4-user-promise.js';
 import uploadPhoto from './5-photo-reject.js';
 
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
