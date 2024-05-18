923cd56aaf2c39f73b32b78aa3fe95ad06b4d9d1
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-17 23:58:43 -0400

    modified 6-final-user.js

--- ES6_promise/6-final-user.js
+++ ES6_promise/6-final-user.js
@@ -1,12 +1,6 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
 export default function handleProfileSignup(firstName = '', lastName = '', fileName = '') {
-  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)])
-    .then((results) => {
-      return results.map((result, index) => {
-        if (index === 0) {
-          return { status: result.status, value: 'error' };
-        } else {
-          return { status: result.status, value: 'user' };
-        }
-      });
-    });
+  return Promise.allSettled([uploadPhoto(fileName), signUpUser(firstName, lastName)]);
 }
