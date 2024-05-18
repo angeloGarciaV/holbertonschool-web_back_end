50a128f3e4ed82f998355055900accb7fd9cf8f9
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-17 23:35:44 -0400

    Added 5-photo-reject.js

--- ES6_promise/0-promise.js
+++ ES6_promise/0-promise.js
@@ -1,11 +1,9 @@
 export default function getResponseFromAPI() {
   return new Promise((resolve, reject) => {
-    /* eslint-disable */
       if (true) {
         resolve();
       } else {
         reject();
       }
-      /* eslint-enable */
   });
 }
