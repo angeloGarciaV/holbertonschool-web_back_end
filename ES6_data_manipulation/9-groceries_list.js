93b822d715579e2cd4b18fe62ad142c8df86f9dd
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-24 12:49:47 -0400

    fix for 9-groceries_list.js

--- ES6_data_manipulation/9-groceries_list.js
+++ ES6_data_manipulation/9-groceries_list.js
@@ -1,9 +1,11 @@
 export default function groceriesList() {
-  return new Map([
+  const map = new Map([
     ['Apples', 10],
     ['Tomatoes', 10],
     ['Pasta', 1],
     ['Rice', 1],
     ['Banana', 5],
   ]);
+
+  return map;
 }
