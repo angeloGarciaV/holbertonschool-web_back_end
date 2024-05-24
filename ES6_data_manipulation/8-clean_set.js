bcc7e60ad5293defaa40dba9870861402b3b0b98
Angelo Garcia <angelogarcia1029@gmail.com>
2024-05-24 12:37:39 -0400

    Error catching for 8-clean_set.js

--- ES6_data_manipulation/8-clean_set.js
+++ ES6_data_manipulation/8-clean_set.js
@@ -1,4 +1,9 @@
 export default function cleanSet(set, startString) {
+  if (
+    typeof set !== 'object' || typeof startString !== 'string' || startString.length === 0) {
+    return '';
+  }
+
   const arr = [];
 
   set.forEach((element) => {
